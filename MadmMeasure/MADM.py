62d841245fca45f681ae97453de263d9ef7f88c2
Thomas Helfer <pr1uhelf@int1.bullx>
2018-05-17 20:33:16 +0200

    Madm not working

--- MadmMeasure/MADM.py
+++ MadmMeasure/MADM.py
@@ -2,13 +2,12 @@ import yt
 from yt import derived_field
 import numpy as np
 from numpy.linalg import inv
-from scipy.interpolate import interp1d
-from scipy.optimize import fsolve
 import time
 import os
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from numpy import pi
 yt.enable_parallelism()
 
 # ==============================================================================
@@ -28,7 +27,15 @@ ds = yt.load('../../*.hdf5')
 centerXYZ =  ds[0].domain_right_edge/2
 center = float(centerXYZ[0])
 
-RadiusADM = 100
+Rad = 100.
+
+NInt = 7
+coord = np.loadtxt('PointDistFiles/lebedev/lebedev_%03d.txt'% NInt)
+theta = coord[:,1]*pi/180; phi = coord[:,0]*pi/180 + pi
+w = coord[:,2]
+
+NumInt = len(theta)
+print("We are using", NumInt, " point over the sphere")
 
 # =================================
 #  INPUT #
@@ -39,10 +46,7 @@ BHcenter = [center,center,center]
 
 time_data = []
 CycleData = []
-AHradius = []
-AHradiusError = []
-BHmass = []
-BHmassError = []
+MADM_data = []
 
 # used interpolation quality
 Quality = 'quadratic'
@@ -50,6 +54,7 @@ Quality = 'quadratic'
 for i in ds:
 
 	start = time.time()
+
 
 	# Gradients calculated by YT
 
@@ -69,149 +74,170 @@ for i in ds:
 
 	print ("Preparing calculation ... ")
 
-	c = i.point([1,2,3])
-	BHcenterYT = i.arr(BHcenter, "code_length")
+	counter = 0
+
+	MADM = 0
+
+	for (k,x) in enumerate(phi):
+		phi_var = phi[k]
+		theta_var = theta[k]
+		x1 = Rad*np.cos(phi_var)*np.sin(theta_var)+float(centerXYZ[0])
+		y1 = Rad*np.sin(phi_var)*np.sin(theta_var)+float(centerXYZ[1])
+		z1 = Rad*np.cos(theta_var)+float(centerXYZ[2])
+		ptnEx = [x1,y1,z1]
+		c = i.point(ptnEx)
+	
+		BHcenterYT = i.arr(BHcenter, "code_length")
 
 	# Yt gives data unsorted
 
-	x = np.array(c["x"]- BHcenterYT[0])
-	y = np.array(c["y"]- BHcenterYT[1])
-	z = np.array(c["z"]- BHcenterYT[2])
+		x = np.array(c["x"]- BHcenterYT[0])
+		y = np.array(c["y"]- BHcenterYT[1])
+		z = np.array(c["z"]- BHcenterYT[2])
 
-	r = np.sqrt(x*x+y*y+z*z)
-	srt = np.argsort(r)
+		r = np.sqrt(x*x+y*y+z*z)
+		srt = np.argsort(r)
 
 	# Importing Coordinates
 
-	x = np.array(c["x"][srt]- BHcenterYT[0])
-	y = np.array(c["y"][srt]- BHcenterYT[1])
-	z = np.array(c["z"][srt]- BHcenterYT[2])
+		x = np.array(c["x"][srt]- BHcenterYT[0])
+		y = np.array(c["y"][srt]- BHcenterYT[1])
+		z = np.array(c["z"][srt]- BHcenterYT[2])
 
-	pos = []
+		pos = []
 
-	pos.append(x)
-	pos.append(y)
-	pos.append(z)
+		pos.append(x)
+		pos.append(y)
+		pos.append(z)
 
-	rho2 = x*x + y*y
-	rho = np.sqrt(rho2)
+		rho2 = x*x + y*y
+		rho = np.sqrt(rho2)
 
-	r2 = rho2 + z*z
-	r = np.sqrt(r2)
+		r2 = rho2 + z*z
+		r = np.sqrt(r2)
 
-	cosphi = x/rho
-	sinphi = y/rho
-	sintheta = rho/r
-	costheta = z/r
+		cosphi = x/rho
+		sinphi = y/rho
+		sintheta = rho/r
+		costheta = z/r
 
-	chi = np.array(c["chi"][srt]);
-	chi2 = chi*chi
-
-
-	g11 = np.array(c["h11"][srt]);
-	g12 = np.array(c["h12"][srt]);
-	g13 = np.array(c["h13"][srt]);
-	g22 = np.array(c["h22"][srt]);
-	g23 = np.array(c["h23"][srt]);
-	g33 = np.array(c["h33"][srt]);
+		chi = np.array(c["chi"][srt]);
+		chi2 = chi*chi
 
 
-	g = [[],[],[]]
+		g11 = np.array(c["h11"][srt]);
+		g12 = np.array(c["h12"][srt]);
+		g13 = np.array(c["h13"][srt]);
+		g22 = np.array(c["h22"][srt]);
+		g23 = np.array(c["h23"][srt]);
+		g33 = np.array(c["h33"][srt]);
+	
+	
+		g = [[],[],[]]
 
-	g[0].append(g11)
-	g[0].append(g12)
-	g[0].append(g13)
-	g[1].append(g12)
-	g[1].append(g22)
-	g[1].append(g23)
-	g[2].append(g13)
-	g[2].append(g23)
-	g[2].append(g33)
+		g[0].append(g11)
+		g[0].append(g12)
+		g[0].append(g13)
+		g[1].append(g12)
+		g[1].append(g22)
+		g[1].append(g23)
+		g[2].append(g13)
+		g[2].append(g23)
+		g[2].append(g33)
 
-	g = np.array(g)
+		g = np.array(g)
 
 # ========================================================
 #	Define normal vector and invert metric
 # ========================================================
 
-	N = len(chi)
-	s = np.zeros([3,N])
+		N = len(chi)
+		s = np.zeros([3,N])
 
-	for IND in range(N):
-		# Definitions
-		# ---------------------
-		s[0,IND] = x[IND]/r[IND]
-		s[1,IND] = y[IND]/r[IND]
-		s[2,IND] = z[IND]/r[IND]
+		for IND in range(N):
+			# Definitions
+			# ---------------------
+			s[0,IND] = x[IND]/r[IND]
+			s[1,IND] = y[IND]/r[IND]
+			s[2,IND] = z[IND]/r[IND]
 
 
 # ========================================================
 #	Derivatives
 # ========================================================
 
-	dgdx = np.zeros([3,3,3,N])
-	dchidx = np.zeros([3,N])
+		dgdx = np.zeros([3,3,3,N])
+		dchidx = np.zeros([3,N])
 
-	print ("Calculating metric x Gradient ... ")
-	dgdx[0,0,0,:] = c["h11_gradient_x"][srt]
-	dgdx[0,1,0,:] = c["h12_gradient_x"][srt]
-	dgdx[0,2,0,:] = c["h13_gradient_x"][srt]
-	dgdx[1,1,0,:] = c["h22_gradient_x"][srt]
-	dgdx[1,2,0,:] = c["h23_gradient_x"][srt]
-	dgdx[2,2,0,:] = c["h33_gradient_x"][srt]
-	dgdx[1,0,0,:] = dgdx[0,1,0,:]
-	dgdx[2,0,0,:] = dgdx[0,2,0,:]
-	dgdx[2,1,0,:] = dgdx[1,2,0,:]
-
-
-	print ("Calculating metric y Gradient ... ")
-	dgdx[0,0,1,:] = c["h11_gradient_y"][srt]
-	dgdx[0,1,1,:] = c["h12_gradient_y"][srt]
-	dgdx[0,2,1,:] = c["h13_gradient_y"][srt]
-	dgdx[1,1,1,:] = c["h22_gradient_y"][srt]
-	dgdx[1,2,1,:] = c["h23_gradient_y"][srt]
-	dgdx[2,2,1,:] = c["h33_gradient_y"][srt]
-	dgdx[1,0,1,:] = dgdx[0,1,1,:]
-	dgdx[2,0,1,:] = dgdx[0,2,1,:]
-	dgdx[2,1,1,:] = dgdx[1,2,1,:]
+		print ("Calculating metric x Gradient ... ")
+		dgdx[0,0,0,:] = c["h11_gradient_x"][srt]
+		dgdx[0,1,0,:] = c["h12_gradient_x"][srt]
+		dgdx[0,2,0,:] = c["h13_gradient_x"][srt]
+		dgdx[1,1,0,:] = c["h22_gradient_x"][srt]
+		dgdx[1,2,0,:] = c["h23_gradient_x"][srt]
+		dgdx[2,2,0,:] = c["h33_gradient_x"][srt]
+		dgdx[1,0,0,:] = dgdx[0,1,0,:]
+		dgdx[2,0,0,:] = dgdx[0,2,0,:]
+		dgdx[2,1,0,:] = dgdx[1,2,0,:]
 
 
-	print ("Calculating metric z Gradient ... ")
-	dgdx[0,0,2,:] = c["h11_gradient_z"][srt]
-	dgdx[0,1,2,:] = c["h12_gradient_z"][srt]
-	dgdx[0,2,2,:] = c["h13_gradient_z"][srt]
-	dgdx[1,1,2,:] = c["h22_gradient_z"][srt]
-	dgdx[1,2,2,:] = c["h23_gradient_z"][srt]
-	dgdx[2,2,2,:] = c["h33_gradient_z"][srt]
-	dgdx[1,0,2,:] = dgdx[0,1,2,:]
-	dgdx[2,0,2,:] = dgdx[0,2,2,:]
-	dgdx[2,1,2,:] = dgdx[1,2,2,:]
+		print ("Calculating metric y Gradient ... ")
+		dgdx[0,0,1,:] = c["h11_gradient_y"][srt]
+		dgdx[0,1,1,:] = c["h12_gradient_y"][srt]
+		dgdx[0,2,1,:] = c["h13_gradient_y"][srt]
+		dgdx[1,1,1,:] = c["h22_gradient_y"][srt]
+		dgdx[1,2,1,:] = c["h23_gradient_y"][srt]
+		dgdx[2,2,1,:] = c["h33_gradient_y"][srt]
+		dgdx[1,0,1,:] = dgdx[0,1,1,:]
+		dgdx[2,0,1,:] = dgdx[0,2,1,:]
+		dgdx[2,1,1,:] = dgdx[1,2,1,:]
+	
 
+		print ("Calculating metric z Gradient ... ")
+		dgdx[0,0,2,:] = c["h11_gradient_z"][srt]
+		dgdx[0,1,2,:] = c["h12_gradient_z"][srt]
+		dgdx[0,2,2,:] = c["h13_gradient_z"][srt]
+		dgdx[1,1,2,:] = c["h22_gradient_z"][srt]
+		dgdx[1,2,2,:] = c["h23_gradient_z"][srt]
+		dgdx[2,2,2,:] = c["h33_gradient_z"][srt]
+		dgdx[1,0,2,:] = dgdx[0,1,2,:]
+		dgdx[2,0,2,:] = dgdx[0,2,2,:]
+		dgdx[2,1,2,:] = dgdx[1,2,2,:]
+	
 
-	print ("Calculating chi Gradient ... ")
-	dchidx[0,:] = c["chi_gradient_x"][srt]
-	dchidx[1,:] = c["chi_gradient_y"][srt]
-	dchidx[2,:] = c["chi_gradient_z"][srt]
-
+		print ("Calculating chi Gradient ... ")
+		dchidx[0,:] = c["chi_gradient_x"][srt]
+		dchidx[1,:] = c["chi_gradient_y"][srt]
+		dchidx[2,:] = c["chi_gradient_z"][srt]
+	
 # ========================================================
 #	Calculation of Omega
 #	Eq. 7.41 in shapiro book
 # ========================================================
 
-
-	print ("Calculating Theta ... ")
-	MADM = np.zeros(N)
+		counter += 1. 
+		print ("Calculating MADM ... ")
+		print ("Percentage ",counter/NumInt*100," %"  )
+		MADMInt = np.zeros(N)
 
 # ----------------------------------------------------------
-	for i in range(3):
-		for j in range(3):
-			MADM += (dgdx[i,j,i]/chi-dchidx[i]/chi**2.*g[i,j])*s[j]
+		for d in range(3):
+			for j in range(3):
+				MADMInt += (dgdx[d,j,d]/chi-dchidx[d]/chi**2.*g[d,j])*s[j]
 
+		for d in range(3):
+			for j in range(3):
+				MADMInt += -(-chi**(-2.)*dchidx[d]*g[j,j]+dgdx[j,j,d]*chi**(-1.))*s[d]
 
-	for i in range(3):
-		for j in range(3):
-			MADM += (-chi**(-2.)*dchidx[i]*g[j,j]+dgdx[j,j,i]*chi**(-1.))*s[i]
+		MADM += MADMInt[0]*w[k]
 
+		print("ADM mass = ", MADM)
 
+	print("MADM mass ", MADM, "at time",i.current_time)
+	
+	MADM_data.append(MADM)
+	time_data.append(i.current_time)	
 	CycleData.append(time.time()-start)
 	np.savetxt('Cycletime.out',CycleData)
+	np.savetxt('time.out',time_data)
+	np.savetxt('MADM.out',MADM_data)
