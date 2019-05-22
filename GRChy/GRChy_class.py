b9103ce33e1236c93dc5c18126de964a9db426a0
Cristian Joana <joana@physik.rwth-aachen.de>
2019-05-22 11:18:28 +0200

    checkpoint_IO for GRChy

--- GRChy/GRChy_class.py
+++ GRChy/GRChy_class.py
@@ -10,16 +10,17 @@ class GRChy():
     def __init__(self, path):
         h5 = h5py.load(path)    # Check
 
-        try:
-            h5.....  (chombo_file)
+        #TODO
+        # try:
+        #     h5.....  (chombo_file)
+        #
+        # except Exception as e:
+        #     # Error message
 
-        except Exception as e:
-            # Error message
+        attrs = np.array(h5.attrs, dtype=str) 
+        max_level = h5.attrs['max_level']
+        fields = attrs[ 'component_' in attrs]
 
-
-        num_levels = h5.....
-
-        fields = h5.... attributes
 
 
     def load_data(self, field):
